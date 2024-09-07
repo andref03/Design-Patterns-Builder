2f3fe4ae7270f7d8d0ec049a82b1df05d13c4063
Andre Felipe <andref.lopes@outlook.com>
2024-09-07 13:33:51 -0300

    primeiro commit de decorator

---
+++ src/main/java/br/ifnmg/edu/designpatternsdecorator/DesignPatternsDecorator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ifnmg.edu.designpatternsdecorator;
+
+/**
+ *
+ * @author ADM
+ */
+public class DesignPatternsDecorator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
